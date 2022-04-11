634f6f77d65fa130ddd250363aca7648415fe8d3
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-04-11 18:08:47 +0700

    tambah materi

---
+++ basic mathematical operations/index.js
@@ -0,0 +1,7 @@
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 5;
+let humanDogRatio = 5;
+// 2. Multiply the two together and store the result in myDogAge
+let myDogAge = myAge + humanDogRatio;
+// 3. Log myDogAge to the console
+console.log(myDogAge);
