6976efc1612760d0a2f08d4c3ebc5368c251fa01
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2022-04-17 23:54:26 +0700

    tambah materi

---
+++ reassigning-and-incrementing/main.js
@@ -0,0 +1,14 @@
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+
+let bonusPoint = 50;
+console.log(bonusPoint);
+
+bonusPoint = bonusPoint + 50;
+console.log(bonusPoint);
+
+bonusPoint = bonusPoint - 75;
+console.log(bonusPoint);
+
+bonusPoint = bonusPoint + 45;
+console.log(bonusPoint);
